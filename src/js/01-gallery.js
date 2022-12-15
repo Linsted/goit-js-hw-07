c0f97a7d5062e25393456984edcc4618907d7f88
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-15 23:20:34 +0200

    markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryDivREF = document.querySelector('.gallery')
+galleryDivREF.innerHTML = `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`
+
+// const galleryMarkup = `<div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>`
+
+
+function createMarkup(galleryItems) {
+    
+}
