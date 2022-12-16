d6712435803eb122625c38275db02f5dfb32f7ec
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-17 00:20:35 +0200

    added prewiev img.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// console.log(galleryItems);
 
-const galleryDivREF = document.querySelector('.gallery')
-galleryDivREF.innerHTML = `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+
+
+
+const galleryDivREF = document.querySelector('.gallery');
+
+
+
+createGalleryMarkup(galleryItems);
+galleryDivREF.innerHTML = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+
+
+  const galleryMarkup = galleryItems.map(({original, preview, description}) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
+      src="${preview}"
       data-source="large-image.jpg"
-      alt="Image description"
+      alt="${description}"
     />
   </a>
-</div>`
-
-// const galleryMarkup = `<div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>`
-
-
-function createMarkup(galleryItems) {
-    
+</div>`).join('')
+  
+  
+  return galleryMarkup;
 }
