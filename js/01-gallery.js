1fd3bf6d2fa94618a828c7de133168199970a9b6
Ivan Malenko <ivan.malenko18@gmail.com>
2022-12-17 15:01:22 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,8 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 //REFS//
 const galleryDivREF = document.querySelector('.gallery');
-const galleryLinkREF = document.querySelector('gallery__link')
-const imgREF = document.querySelector('img')
 //  --  //
 
 
@@ -19,8 +17,7 @@ galleryDivREF.addEventListener('click', getFullImgLink);
 
 function createGalleryMarkup(items) {
 
-
-  const galleryMarkup = galleryItems.map(({original, preview, description}) => `<div class="gallery__item">
+ const galleryMarkup = galleryItems.map(({original, preview, description}) => `<div class="gallery__item">
   <a class="gallery__link">
     <img
       class="gallery__image"
@@ -28,8 +25,8 @@ function createGalleryMarkup(items) {
       data-source="${original}"
       alt="${description}"
     />
-  </a>
-</div>`).join('')
+   </a>
+  </div>`).join('')
   
   
   return galleryMarkup;
@@ -38,21 +35,17 @@ function getFullImgLink(event) {
   if (event.target.nodeName !== 'IMG') return;
 
 
+
+event.target.classList.add('is-active')
   const instance = basicLightbox.create(`<img
       class="gallery__image is-active"
       src="${event.target.dataset.source}"
-    />`).show();
+    />`,).show();
   
   
   
 };
 
-// document.addEventListener('keydown', (event) => {
-//     console.log(event.code);
-//     if (event.code === `KeyQ`) { instance.close()};
-//   });
-
-///
 
 
 
